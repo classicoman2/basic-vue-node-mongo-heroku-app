679c71174859bed4fcd6e5a2cd069634584dc513
‘classicoman2’ <aamengual@cifpfbmoll.eu>
2021-03-24 14:58:45 +0100

    1st commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+// server.js
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+//express i cors
+const express = require("express");
+const cors = require("cors");
+
+server.use(express.static("server/public"))
+
+server.use(middlewares);
+
+server.get("/", function (req, res) {
+  res.sendFile(path.dirname(path.dirname(__dirname)) + "/public/index.html");
+});
+
+server.use(router);
+
+// HEROKU 
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`JSON Server is running - http://localhost:${port}`);
+});
